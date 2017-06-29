3765af9f508bb18d00ab51be45647fa936006fba
Taylor Holberton <taylorcholberton@gmail.com>
2017-06-28 20:43:59 -0700

    removed prototypes not needed in derived class

--- include/tinyalsa/input-pcm.hpp
+++ include/tinyalsa/input-pcm.hpp
@@ -12,9 +12,7 @@ class InputPcm : public Pcm {
 public:
 	static std::shared_ptr<InputPcm> Create(void);
 	virtual ~InputPcm(void);
-	virtual const Config& GetConfig(void) const = 0;
 	virtual size_t Read(void* frame_array, size_t frame_count) = 0;
-	virtual void SetConfig(const Config& config) = 0;
 };
 
 } /* namespace tinyalsa */
